3284b9b4c04264c2083b95b4e1d1f3592bc6ffc8
jaeeun <jaeoun0238@naver.com>
2024-12-26 16:23:05 +0900

    Update user.router
    로그아웃 부분 업데이트

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import userRouter from './routers/user.router.js';
 import errorMiddleware from './middlewares/error.middleware.js';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
-
 const PORT = 3000;
 
 app.use(express.json());
--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import { prisma } from '../uts/prisma/index.js';
+import errModel from '../middlewares/error.middleware.js';
+import authMiddleware from '../middlewares/auth.middleware.js';
 import bcrypt from 'bcrypt';
 import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
@@ -18,6 +20,10 @@ router.post('/sign-up', async (req, res) => {
     userName,
     nickname,
   });
+
+  //  - [ ]  **이메일 가입 및 인증 기능**
+  //  - 이메일 가입 시 이메일 인증 기능을 포함하는 것이 좋습니다.
+  //  - [Nodemailer](https://github.com/nodemailer/nodemailer)와 같은 패키지 사용을 하여 완료해보세요!
 
   // 이메일 중복 체크
   const isExistEmail = await prisma.users.findUnique({
@@ -77,34 +83,48 @@ router.post('/sign-in', async (req, res) => {
   // 입력받은 사용자의 비밀번호와 데이터베이스에 저장된 비밀번호를 비교합니다.
   else if (!(await bcrypt.compare(password, accountData.password)))
     return res.status(401).json({ message: '비밀번호가 일치하지 않습니다.' });
+  console.log('Secret Key:', process.env.SERVER_ACCESS_KEY);
 
   // 비밀번호를 암호화(bcrypt 사용)하여 저장.
   // 로그인에 성공하면, 사용자의 accountId를 바탕으로 토큰을 생성합니다.
-  // const accesstoken = jwt.sign(
-  //   {
-  //     userId: accountData.userId,
-  //   },
-  //   // JWT를 서명하는 데 사용되는 비밀 키
-  //   // 서버가 비밀 키를 사용하여 토큰 변조 여부를 알 수 있다
-  //   process.env.SERVER_ACCESS_KEY,
-  //   // 엑세스 토큰
-  //   { expiresIn: '5m' },
-  // );
+  const token = jwt.sign(
+    {
+      userId: accountData.userId,
+    },
 
-  res.cookie('userId', accountData.userId, {
-    httpOnly: true, // XSS 공격 방지
-    secure: process.env.NODE_ENV === 'production', // HTTPS에서만 전송 (배포 환경에서만 적용)
-    maxAge: 5 * 60 * 1000, // 쿠키 만료 시간 설정 (5분)
-  });
-  return res.status(200).json({
-    message: '로그인 성공',
-    email: accountData.email,
-  });
+    // JWT를 서명하는 데 사용되는 비밀 키
+    // 서버가 비밀 키를 사용하여 토큰 변조 여부를 알 수 있다
+    process.env.JWT_SECRET,
+    // 엑세스 토큰
+    { expiresIn: '5m' },
+  );
+
+  res.cookie('userId', `Bearer ${token}`);
+  return res
+    .status(200)
+    .json({ message: '로그인 성공', email: accountData.email });
+
+  // res.cookie('userId', accountData.userId, {
+  //   httpOnly: true, // XSS 공격 방지
+  //   secure: process.env.NODE_ENV === 'production', // HTTPS에서만 전송 (배포 환경에서만 적용)
+  //   maxAge: 10 * 60 * 1000, // 쿠키 만료 시간 설정 (10분)
+  // });
+  // 로그인시 jwt로 인증하고 쿠키에 저장하기
+  // 쿠키를 받아올수있도록 로직 추가해야함
+  // 1. 로그인 했을때 jwt 생성과 동시에 쿠키를 저장
+  // 2. 클라이언트는 jwt를 서버로 전송
+  // 3. 서버는 jwt를 확인하여 사용자 검증
 });
 
 // 로그아웃
 router.post('/sign-out', (req, res) => {
-  res.clearCookie('userId'); // 쿠키 삭제
-  return res.status(200).json({ message: '로그아웃 성공' });
+  try {
+    // authorization 쿠키를 삭제하여 로그아웃 처리
+    res.clearCookie('userId');
+    return res.status(200).json({ message: '로그아웃 성공' });
+  } catch (error) {
+    next(error); // 에러 핸들링 미들웨어로 전달
+  }
 });
+
 export default router;
