758cd428af03b100605bf07b7b12f77518aeff22
강나연 <skdusdl4@naver.com>
2024-12-26 20:15:24 +0900

    [docs] app.js 파일 수정
    app.js 파일 수정

--- src/app.js
+++ src/app.js
@@ -1,24 +1,31 @@
 import express from 'express';
 import userRouter from './routers/user.router.js';
-import errorMiddleware from './middlewares/error.middleware.js';
-import dotenv from 'dotenv';
 import userinfoRouter from './routers/userinfo.router.js';
+import errorMiddleware from './middlewares/error.middleware.js';
+import profileRouter from './routers/profiles.router.js';
+import postRouter from './routers/posts.router.js';
+import duoReviewRouter from './routers/duoreview.router.js';
+import commentRouter from './routers/comments.router.js';
+
+import dotenv from 'dotenv';
 
 dotenv.config();
+
 const app = express();
 const PORT = 3000;
 
-// JSON 파싱 미들웨어 설정
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-// '/api' 경로에 여러 라우터 연결
-app.use('/api', [userRouter, userinfoRouter]);
-
-// 에러 처리 미들웨어
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', [
+  userRouter,
+  userinfoRouter,
+  profileRouter,
+  postRouter,
+  duoReviewRouter,
+  commentRouter,
+]);
 app.use(errorMiddleware);
 
-// 서버 시작
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
 });
